2d151be2fc8445cf30d1b9235a8b72b50fe39771
Debanjali Saha <sdebanjali2@gmail.com>
2026-01-07 10:04:03 +0530

    Initial commit: python variables practice

---
+++ basics/variables.py
@@ -0,0 +1,55 @@
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+Sub = input("Enter your favourite subject:")
+print ("My name is:",name,"\nMy age is",age, "\nMy favourite subject is",age)
+
+a = int(input("Input a number:"))
+b = int(input("Input another number:"))
+sum = a+b
+dif = a-b
+mul = a*b
+print("the sum of these two num is:",sum,"\nthe difference of these two number is:",dif, "\nthe multiplication of these two number is:",mul)
+
+a= 3
+b= 3.3
+c= "ddd"
+d= True
+print(type(a),type(b),type(c),type(d))
+
+score= 10
+score+= 5
+print("the value of score is", score)
+
+a=5
+b=10
+a=(a+b)
+b=a-b
+a=a-b
+print ("thr value of a and b is:",a,b)
+
+first_name= input("Enter your first name ")
+second_name= input("Enter your second name ")
+full_name= first_name +" "+second_name
+print("My full name is:",full_name)
+
+print("My name is",name,"and I am", age,"years old")
+
+p= int(input("Enter the amt of principal"))
+r= int(input("Enter the rate of interest"))
+t= int(input("Enter the time"))
+SI= (p*r*t)/100
+print("the Simple interest is:", SI)
+
+x=10
+y=x
+x=20
+print("X=",x, "\ny=",y)
+
+r= int(input("enter the radius of the circle:",))
+area= 3.14*r**2
+print("the area of the circle is:",area)
+
+total_marks=70
+obtaind_marks=int(input("Enter your obtained marks:"))
+percentage=(obtaind_marks/total_marks)*100
+print("the percentage of your marks is:", percentage)
---
+++ functions/functions_intro.py
@@ -0,0 +1,5 @@
+def sum(a,b):
+    ans = a+b
+    return ans
+n= sum(2,3)
+print(n)
